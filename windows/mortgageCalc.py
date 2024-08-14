0fd3e08b4c5d093e9d23fde9fcf8832e6f40de48
Tu <tun42092@temple.edu>
2024-08-14 18:43:58 -0400

    finished first iteration

--- windows/mortgageCalc.py
+++ windows/mortgageCalc.py
@@ -1,18 +1,44 @@
 from tkinter import *
+import sys
+import os
+
+# Step 2: Get the parent directory path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Step 4: Append the parent directory to sys.path
+sys.path.append(parent_dir)
+
 from mtgCalculator_AP import MortgageCalculator
-
-
 
 
 
 #functions
 
 def calc_level_payment():
-    pass
+    
+    #need to check if the user has enter a character other than numbers
+    #need to check if the user has enter nothing
 
-    loanAmount = lbl_loanAmount.get()
-    #mtgCalc = MortgageCalculator()
-
+    loanAmount = ent_loanAmount.get().strip()
+    
+    interestRate = ent_interestRate.get().strip()
+    term = ent_mortgageTerm.get().strip()
+    
+    
+    
+    if loanAmount == "" or interestRate == "" or term == "":
+        lbl_error["text"] = "Please fill out all boxes"
+    
+    elif not loanAmount.isdigit() or not term.isdigit():
+        lbl_error["text"] = "Please enter only numbers"
+    else:
+        lbl_error["text"] = ""
+        loanAmount = int(loanAmount)
+        interestRate = float(interestRate)
+        term = int(term)
+        mtgCalc = MortgageCalculator(loan_amount= loanAmount, int_rate= interestRate, term= term)
+        lbl_total["text"] = f"{mtgCalc.calc_level_payment()}"
+        
 ##predefined
 large_font = ("Helvetica", 16)
 
@@ -49,8 +75,15 @@ ent_mortgageTerm = Entry(master = frm_inputs)
 lbl_mortgageTerm.grid(row= 2, column= 0, sticky= "e")
 ent_mortgageTerm.grid(row = 2, column= 1, sticky= "w")
 
-btn_enter = Button(window, text = "Calculate")
+btn_enter = Button(window, text = "Calculate", command= calc_level_payment)
 btn_enter.pack()
+
+lbl_total = Label()
+lbl_total.pack()
+
+lbl_error = Label()
+lbl_error.pack()
+
 
 
 
